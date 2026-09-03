d4664e1e0a81f4bbf98663c4369049683e65838b
Dylan Moragrega <dylanvladimir19@gmail.com>
2026-09-03 10:46:28 -0700

    Primer commit: servidor Spring Boot con endpoints GET y POST

---
+++ src/test/java/com/moragrega/MiPrimerServidorWeb/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moragrega.MiPrimerServidorWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
